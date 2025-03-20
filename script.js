b8fdfe8ef19eb4daa4b89ca9048d787f5115040e
Adezaren <160015024+Adezaren@users.noreply.github.com>
2025-03-20 19:36:35 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const apiKey = '4cce2b5f82d26d6c819c6c22486071ef';
+
+const fetchRate =  async (targetCurrency) => {
+
+    const url = `http://data.fixer.io/api/latest?access_key=${apiKey}`;
+
+    const response = await fetch(url);
+        const data = await response.json();
+
+        if (!data.success) {
+            throw new Error('Chyba při načítání');
+        }
+
+        const rate = data.rates[targetCurrency];
+       
+
+        return rate;
+};
+
+const convertCurrency = async () => {
+    const amount = parseFloat(document.getElementById('amount').value);
+    const targetCurrency = document.getElementById('currency').value;
+    
+    if (isNaN(amount) || amount <= 0) {
+        alert('Zadejte kladnou sumu.');
+        return;
+    }
+
+    const rate = await fetchRate(targetCurrency);
+    if (rate) {
+        const convertedAmount = amount * rate;
+        document.getElementById('result').textContent = `${amount} EUR = ${convertedAmount.toFixed(2)} ${targetCurrency}`;
+    }
+};
+
+async function fetchCurrencies() {
+    
+        const response = await fetch(`https://api.exchangerate.host/symbols?access_key=${apiKey}`);
+        const data = await response.json();
+        const currencySelect = document.getElementById('currency');
+        
+        for (const currency in data.symbols) {
+            let option = document.createElement('option');
+            option.value = currency;
+            option.textContent = `${currency} - ${data.symbols[currency]}`;
+            currencySelect.appendChild(option);
+        }
+    
+}
+
+fetchCurrencies();
+
